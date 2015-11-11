dadc86b2681c1217728c350d4493e6b8c52424dc
Brett Wise <brettdwise@gmail.com>
2015-11-10 22:00:32 -0500

    Added jsx decoding of github response

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -6,13 +6,21 @@ defmodule Issues.GithubIssues do
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
-
   end
+
+  def handle_response(%{status_code: 200, body: body}) do
+    {:ok, :jsx.decode(body)}
+  end
+
+  def handle_response(%{status_code: ___, body: body}) do
+    {:error, :jsx.decode(body)}
+  end
+
+  # use a module attribute to fetch the value at compile time
+  @github_url Application.get_env(:issues, :github_url)
 
   def issues_url(user, project) do
     "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response(%{status_code: 200, body: body}), do: {:ok, body}
-  def handle_response(%{status_code: _, body: body}), do: {:error, body}
 end
