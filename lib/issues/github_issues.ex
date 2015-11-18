957f70cef4074c1b9fca08bc726d678168d24187
Brett Wise <brettdwise@gmail.com>
2015-11-18 17:07:21 -0500

    Added logging and subsequently debugging

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,10 +1,15 @@
 defmodule Issues.GithubIssues do
 
+  require Logger
+
   @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
 
   def fetcher(user, project) do
+    Logger.info "Fetching user #{user}'s project: #{project}'"
     case HTTPoison.get(issues_url(user, project), @user_agent) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        Logger.info "Successful response"
+        Logger.debug fn -> inspect(body) end
         Poison.decode!(body)
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         IO.puts "Not found :("
